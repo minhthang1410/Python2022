8f6ebaf3e4003d9fa654d12fa74d84425c95922c
minhthang1410 <minhthang14101998@gmail.com>
2022-04-21 01:06:01 +0700

    Add TCP Chat Room Script

--- NetworkPrograming/TCPChatRoom/client.py
+++ NetworkPrograming/TCPChatRoom/client.py
@@ -8,6 +8,7 @@ nickname = input("Choose your nickname: ")
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(('127.0.0.1', 55555))
 
+
 # Listening to Server and Sending Nickname
 def receive():
     while True:
@@ -24,13 +25,15 @@ def receive():
             print("An error occured!")
             client.close()
             break
-        
+
+
 # Sending Messages To Server
 def write():
     while True:
         message = '{}: {}'.format(nickname, input(''))
         client.send(message.encode('ascii'))
-        
+
+
 # Starting Threads For Listening And Writing
 receive_thread = threading.Thread(target=receive)
 receive_thread.start()
--- NetworkPrograming/TCPChatRoom/server.py
+++ NetworkPrograming/TCPChatRoom/server.py
@@ -1,4 +1,3 @@
-from email import message
 import threading
 import socket
 
@@ -15,11 +14,13 @@ server.listen()
 clients = []
 nicknames = []
 
+
 # Sending Messages To All Connected Clients
 def broadcast(message):
     for client in clients:
         client.send(message)
-        
+
+
 # Handling Messages From Clients
 def handle(client):
     while True:
@@ -36,7 +37,8 @@ def handle(client):
             broadcast('{} left!'.format(nickname).encode('ascii'))
             nicknames.remove(nickname)
             break
-        
+
+
 # Receiving / Listening Function
 def receive():
     while True:
@@ -58,5 +60,7 @@ def receive():
         # Start Handling Thread For Client
         thread = threading.Thread(target=handle, args=(client,))
         thread.start()
-        
+
+
+print("Server is listening !")
 receive()
